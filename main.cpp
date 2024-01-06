787de8114f66d12db079b654e3ea1e903c849886
fotis222 <nitsas2012@gmail.com>
2024-01-06 15:47:33 +0200

    decided, added hypotenuse calc

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <limits>
+#include <cmath>
 
 int main()
 {
-    std::cout << "placeholder";
+    std::cout << "select a mode: \n(1) hypotenuse calc\nmore coming soon...\n";
+    std::string mode{};
+    std::cin >> mode;
+    if (mode == "1")
+    {
+        double a, b;
+        std::cout << "base: \n";
+        std::cin >> a;
+        std::cout << "height: \n";
+        std::cin >> b;
+        std::cout << "hypotenuse: " << hypot(a, b) << '\n';
+    }
+    else
+    {
+        std::cout << "enter the number in the parentheses of the mode you want\n";
+    }
+
+    std::cout << "press enter until exit";
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     std::cin.get();
